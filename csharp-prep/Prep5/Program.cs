9fac9dff1322e43b8b170a8feb64b973e4be8a48
Bryan Nielsen <bryniel48@gmail.com>
2023-04-26 10:58:28 -0600

    Finished Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,36 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+    }
+    static String PromptUserName(){
+        Console.WriteLine("Enter your name:");
+        String name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber(){
+        Console.WriteLine("Enter your favorite number:");
+        String favNumString = Console.ReadLine();
+        int favNum = Convert.ToInt32(favNumString);
+        return favNum;
+    }
+    static int SquareNumber(int num){
+        int newNum = num * num;
+        return newNum;
+    }
+    static void DisplayInfo(String name, int num){
+        Console.WriteLine($"{name}, your favorite number squared is {num}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        String name = PromptUserName();
+        int favNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(favNumber);
+
+        DisplayInfo(name, squaredNumber);
     }
 }
